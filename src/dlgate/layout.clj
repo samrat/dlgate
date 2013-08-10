3be99333bff2998dd960d9755870f29d908f3ba9
Samrat Man Singh <samratmansingh@gmail.com>
2013-08-10 21:48:01 +0545

    Polish layout and add social buttons.

--- src/dlgate/layout.clj
+++ src/dlgate/layout.clj
@@ -2,6 +2,43 @@
   (:require [hiccup.core :refer [html]]
             [hiccup.page :refer [html5 include-css include-js]]))
 
+(def tweet-button
+  "<a href='https://twitter.com/share' class='twitter-share-button'
+  data-url='http://dlgate.samrat.me' data-text='dlgate- Transfer files
+  from the web to your Copy.com folder'>Tweet</a>
+  <script>!function(d,s,id){var
+  js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,
+  'script', 'twitter-wjs');</script>")
+
+(def flattr
+  "<script id='flattrbtn'>(function(i){var
+  f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=samrat&button=compact&url='+encodeURIComponent(document.URL);f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('flattrbtn');</script>")
+
+(def fb-javascript-sdk
+  "<div id='fb-root'></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = '//connect.facebook.net/en_US/all.js#xfbml=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>")
+
+(def fb-button
+  "<div class='fb-like' data-href='http://dlgate.samrat.me' data-width='100' data-layout='button_count' data-show-faces='false' data-send='false'></div>")
+
+(def google-analytics
+  "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-34425817-3', 'samrat.me');
+  ga('send', 'pageview');
+
+</script>")
+
 (defn common
   [title & body]
   (html5
@@ -13,9 +50,30 @@
     (include-css "/Gumby/css/gumby.css")
     (include-css "/css/layout.css")]
    [:body
+    fb-javascript-sdk
     [:header {:class "row"}
-     [:h1 "dlgate"]]
-    body]))
+     [:h1 [:a {:href "/"}
+           [:span {:style "color:black;"}
+            "dl"] "gate"]]
+     [:h4 "Transfer files from the web to your Copy.com folder"]
+     [:div {:class "social row"}
+      [:div {:class "three columns"}]
+      [:table {:class "six columns"}
+       [:tr [:td fb-button]
+        [:td tweet-button]
+        [:td flattr]]]]]
+    body
+    
+    [:footer {:class "row"}
+     [:div {:class "three columns"}]
+     [:div {:class "six columns"}
+      [:hr]
+      [:h5 "A "
+       [:a {:href "http://samrat.me"}
+        "Samrat Man Singh"]
+       " production. Made in Nepal."]]]
+
+    google-analytics]))
 
 (defn alert-box
   [alert-type message]
@@ -27,15 +85,15 @@
 
 (defn index
   [& {:keys [alert]}]
-  (common "dlgate"
+  (common "dlgate- Transfer files from the web to your Copy.com folder"
           (html [:div {:clss "twelve colgrid"}
                  (when-not (nil? alert)
                    (alert-box "info" alert))
                  [:div {:class "row"}
                   [:div {:class "three columns"}]
                   [:div {:class "six columns"}
-                   "dlgate lets you send files to your Copy.com folder by simply pasting
-  a URL. To get started, "
+                   "dlgate lets you send files from the web to your Copy.com folder by
+  simply pasting a URL. To get started,"
                    [:a {:href "/login"} "login "]
                    " or "
                    [:a {:href "https://copy.com?r=cPK0qZ"} " join Copy.com"]
@@ -59,12 +117,26 @@
 
 (defn logged-in
   [account-info & {:keys [alert prev-downloads]}]
-  (common "dlgate"
+  (common "dlgate- Transfer files from the web to your Copy.com folder"
           (html [:div {:class "twelve colgrid"}
                  (when-not (nil? alert)
                    (alert-box "success" alert))
+                 
                  [:div {:class "row"}
                   [:div {:class "three columns"}]
+                  [:div {:class "six columns"}
+                   "You're logged in as "
+                   [:strong (:first_name account-info)
+                    " "
+                    (:last_name account-info)
+                    ". "]
+                   "You have "
+                   (free-space account-info)
+                   " GB space left on your Copy folder."]]
+                 [:br]
+                 [:div {:class "row"}
+                  [:div {:class "three columns"}]
+                 
                   [:form {:action "/q"
                           :method "post"
                           :class "append field six columns"}
@@ -80,30 +152,23 @@
                  [:div {:class "row"}
                   [:div {:class "three columns"}]
                   [:div {:class "six columns"}
-                   "You're logged in as "
-                   [:strong (:first_name account-info)
-                    " "
-                    (:last_name account-info)
-                    ". "]
-                   "You have "
-                   (free-space account-info)
-                   " GB space left on your Copy folder."
-                   [:br]
                    (when-not (nil? prev-downloads)
                      [:div
                       [:div {:class "row"}
                        [:h4 "Your most recent downloads"]]
-                      (for [download prev-downloads]
-                        [:div {:class "row"}
-                         [:a {:href (:url download)}
-                          (let [filename (:filename download)
-                                len (count (:filename download))]
-                            (if (> len 25)
-                              (str (subs filename 0 10)
-                                   "..."
-                                   (subs filename
-                                         (- len 12) len))
-                              (:filename download)))]
-                         (status-label (str (:status download)))
-                         [:hr]])])]]])))
+                      [:table {:class ""}
+                       [:tbody
+                        (for [download prev-downloads]
+                          [:tr
+                           [:td [:a {:href (:url download)}
+                                 (let [filename (:filename download)
+                                       len (count (:filename download))]
+                                   (if (> len 25)
+                                     (str (subs filename 0 10)
+                                          "..."
+                                          (subs filename
+                                                (- len 12) len))
+                                     (:filename download)))]]
+                           [:td
+                            (status-label (str (:status download)))]])]]])]]])))
 
